06f6943c02642518745951cf9721fb24d71005a2
Steven G. Harms <steven.harms@gmail.com>
2014-01-28 12:13:26 -0800

    Implement the post 'get_cool_url' route

--- source/sinatra_sandbox/app/controllers/index.rb
+++ source/sinatra_sandbox/app/controllers/index.rb
@@ -4,6 +4,13 @@ get '/' do
   erb :index
 end
 
+post '/get_cool_url' do
+  puts "[LOG] Posted /"
+  puts "[LOG] Params: #{params.inspect}"
+  @genre_name = params[:group1]
+  erb :response
+end
+
 get '/get_cool_url' do
   @myname = params[:myname]
   @radiobutton = params[:group1]
@@ -18,3 +25,4 @@ post '/post_cool_url' do
   puts "[LOG] Params: #{params.inspect}"
   erb :post_cool_url
 end
+
