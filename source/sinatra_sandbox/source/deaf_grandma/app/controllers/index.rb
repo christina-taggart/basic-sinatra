b427164bee4421314d631a4af32b70dfcb97f719
Emily Foley & Kevin Yeh <pair+EmFoley.kyeh@devbootcamp.com>
2014-01-27 21:10:19 -0800

    Attempted to answer 2 questions, still can't get correct db name for psql

--- source/sinatra_sandbox/source/deaf_grandma/app/controllers/index.rb
+++ source/sinatra_sandbox/source/deaf_grandma/app/controllers/index.rb
@@ -1,6 +1,6 @@
 get '/' do
   @grandma = params[:grandma]
-  @mood = params[:mood]
+  @mood = params["post"][:mood] #post button name is a string, value is actual value of mood
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
   erb :index
@@ -8,13 +8,20 @@ end
 
 post '/grandma' do
   @grandma = GrandmaSpeaker.new(params[:user_input]).speak!
-  @mood = params[:mood]
+  @mood = params["post"][:mood]
   puts "[LOG] Posting /grandma"
   puts "[LOG] Params: #{params.inspect}"
   erb :index
 end
 
-
+get '/:mood/:message' do
+  puts "[LOG] params[:mood], params[:message]"
+  @grandma = GrandmaSpeaker.new(params[:message]).speak!
+  @mood = params[:mood] #post button name is a string, value is actual value of mood
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
 
 
 
--- source/sinatra_sandbox/source/deaf_grandma/config/database.rb
+++ source/sinatra_sandbox/source/deaf_grandma/config/database.rb
@@ -26,6 +26,7 @@ end
 db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
 
 DB_NAME = db.path[1..-1]
+p " [LOG] #{DB_NAME} --------------------------------------------------"
 
 # Note:
 #   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
