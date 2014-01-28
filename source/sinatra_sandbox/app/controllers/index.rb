dce082e1d007355bf3346296fef87aec61f6e664
Aditya Mahesh & Darcey Lachtman <pair+amahesh0501.dslachtman@devbootcamp.com>
2014-01-27 18:32:32 -0800

    played around, will work more later

--- source/sinatra_sandbox/app/controllers/index.rb
+++ source/sinatra_sandbox/app/controllers/index.rb
@@ -1,4 +1,20 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/get_cool_url' do
+  @myname = params[:myname]
+  @radiobutton = params[:group1]
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/post_cool_url' do
+  @input = params[:textform]
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_cool_url
 end
